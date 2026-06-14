f79c144f672af9eeb6fc4b28026a82ca1cca96d8
wesleyromanely-lang <wesley.romanely@gmail.com>
2026-06-14 00:30:45 -0300

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ modelos/deluxe/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "deluxe-beauty-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+
+        return cache.addAll(urlsToCache);
+
+      })
+
+  );
+
+});
+
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then((response) => {
+
+        return response || fetch(event.request);
+
+      })
+
+  );
+
+});
